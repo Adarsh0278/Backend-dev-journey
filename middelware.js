6b15197ff614e767e4b40f6876fa35bcd12b0907
Adarsh <183600088+Adarsh0278@users.noreply.github.com>
2026-03-16 16:03:41 +0530

    day-8

--- middelware.js
+++ middelware.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+
+app.use(express.json());
+app.use(cors());
 const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   console.log("working");
@@ -18,6 +22,12 @@ app.get("/about", (req, res) => {
   const b = parseInt(req.query.b);
   const sum = a + b;
   res.send(`The sum of ${a} and ${b} is ${sum}`);
+});
+
+app.post("/sum", (req, res) => {
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+  res.json({ sum: a + b });
 });
 
 app.listen(3000, () => {
